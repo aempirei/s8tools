bd914c89be1df8d26e5f4c25219eed8865240026
Christopher Abad <christopher@abad.ml>
2018-09-25 19:02:43 -0700

    .

--- src/hull.c
+++ src/hull.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <math.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
+int is_maxima(const int filter[3]) {
+	return filter[1] > filter[0] && filter[1] > filter[2];
+}
+
+int is(const int filter[3]) {
+	return filter[2];
+}
+
+int pushift(int filter[3], int x) {
+	int y = filter[0];
+	if(x != filter[2]) {
+		filter[0] = filter[1];
+		filter[1] = filter[2];
+		filter[2] = x;
+	}
+	return y;
+}
+
 int main() {
-	int c;
-	while((c = getchar()) != EOF) {
+	int ch;
+	int maxima = INT8_MIN;
+	int filter[3] = { INT8_MIN, INT8_MIN, INT8_MIN };
+	while((ch = getchar()) != EOF) {
+		pushift(filter, ch);
+		if(is_maxima(filter))
+			maxima = is(filter);
+		putchar(maxima);
 	}
 	return 0;
 }
