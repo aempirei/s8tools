6ba7fdd3466ad399e08be33342c5041c5ca9a440
Christopher Abad <christopher@abad.ml>
2018-10-15 04:54:50 -0700

    .

--- src/reverse.c
+++ src/reverse.c
@@ -7,15 +7,17 @@
 #include <unistd.h>
 
 int main() {
+	const size_t sz = 1 << 20;
 	char *s, *p;
 	int ch;
-	if((s = p = malloc(1 << 20)) == NULL) {
+	if((s = p = malloc(sz)) == NULL) {
 		perror("malloc()");
 		return -1;
 	}
-	while((ch = getchar()) != EOF && p != (s + sizeof(s)))
+	while((ch = getchar()) != EOF && p < s + sz)
 		*p++ = ch;
 	while(p-- > s)
 		putchar(*p);
+	free(s);
 	return 0;
 }
