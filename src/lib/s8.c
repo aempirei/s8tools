20f2f0d142b658a12b14709e8a50c3a00cab640d
Christopher Abad <christopher@abad.ml>
2018-10-15 17:15:57 -0700

    .

--- src/lib/s8.c
+++ src/lib/s8.c
@@ -7,35 +7,51 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <s8.h>
 
-const char *s8_context_dirname() {
-	static char s[NAME_MAX];
+static inline const char *nulltoempty(const char *s) {
+	return s ? s : "";
+}
+
+const char *s8_context_basename() {
+	static char s[NAME_MAX] = "/tmp/s8-fifo.";
 	static bool done = false;
 	if(!done) {
-		snprintf(s, sizeof(s), "/tmp/s8-%d", getppid());
+		snprintf(s, sizeof(s), "/tmp/s8-fifo.%s", nulltoempty(getenv("S8_PREFIX")));
 		done = true;
-	};
+	}
 	return s;
 }
 
 const char *s8_context_filename(char *p, size_t sz, const char *key) {
-	snprintf(p, sz, "%s-%s", s8_context_dirname(), key);
+	snprintf(p, sz, "%s%s", s8_context_basename(), key);
 	return p;
 }
 
-FILE *s8_writer(const char *key) {
+FILE *s8_open(const char *key, const char *mode) {
+	struct stat sb;
 	char filename[NAME_MAX];
 	s8_context_filename(filename, sizeof(filename), key);
-	if(mkfifo(filename, 0700) == -1)
+	if(mkfifo(filename, 0700) == -1 && errno != EEXIST)
 		return NULL;
-	return fopen(filename, "w");
+	if(stat(filename, &sb) == -1)
+		return NULL;
+	if((sb.st_mode & S_IFMT) != S_IFIFO) {
+		errno = ENOSTR;
+		return NULL;
+	}
+	return fopen(filename, mode);
 }
 
-FILE *s8_reader(const char *key) {
-	char filename[NAME_MAX];
-	s8_context_filename(filename, sizeof(filename), key);
-	return fopen(filename, "r");
+int s8_close(FILE *f, const char *key, const char *mode) {
+	int n = fclose(f);
+	if(strcmp(mode, "r") == 0) {
+		char filename[NAME_MAX];
+		s8_context_filename(filename, sizeof(filename), key);
+		n = unlink(filename);
+	}
+	return n;
 }
 
 int s8_bank_init(char *s, int n, FILE *f) {
--- src/lib/s8.h
+++ src/lib/s8.h
@@ -3,9 +3,10 @@
 
 int s8_bank_init(char *, int, FILE *);
 int s8_bank_shift(char *, int, FILE *);
-const char *s8_context_dirname();
+
+const char *s8_context_basename();
 const char *s8_context_filename(char *, size_t, const char *);
-FILE *s8_writer(const char *);
-FILE *s8_reader(const char *);
+FILE *s8_open(const char *, const char *);
+int s8_close(FILE *, const char *, const char *);
 
 #endif
