e92e52c1129be980f044f7ea092e2c8651fc4e49
Christopher Abad <christopher@abad.ml>
2018-10-15 20:56:27 -0700

    .

---
+++ src/r.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+#include <s8.h>
+
+int main(int argc, char **argv) {
+
+	const size_t N = argc - 1;
+	FILE *f[N];
+	int ch;
+
+	if(s8_io_open_all(f, &argv[1], N, "r") == -1) {
+		perror("s8_io_open_all()");
+		exit(-1);
+	}
+
+	for(size_t k = 0; k < N; k++)
+		while((ch = fgetc(f[k])) != EOF)
+			putchar(ch);
+
+	if(s8_io_close_all(f, argv+1, N, "r") == -1) {
+		perror("s8_io_close_all()");
+		exit(-1);
+	}
+
+	exit(0);
+}
