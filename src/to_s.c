44ed106d7fc988f7cc2f19e0bc2f469c5b400257
Christopher Abad <christopher@abad.ml>
2018-10-19 04:21:23 -0700

    .

--- src/to_s.c
+++ src/to_s.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <s8.h>
-int main() {
+int main(int argc, char **argv) {
+	const char *fmt = argc > 1 ? argv[1] : "s";
+	const char *p = fmt;
 	int ch;
-	while((ch = getchar()) != EOF)
-		printf("%d\n", (char)ch);
+
+	for(const char *q = fmt; *q; q++) {
+		if(strchr("su", *q) == NULL) {
+			fprintf(stderr, "unexpected character '%c' in format string \"%s\"\n", *q, fmt);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	while((ch = getchar()) != EOF) {
+		printf("%d", *p++ == 's' ? (char)ch : ch);
+		putchar(*p ? ' ' : '\n');
+		if(*p == '\0')
+			p = fmt;
+	}
 	return 0;
 }
