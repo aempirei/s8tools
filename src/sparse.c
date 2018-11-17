50e6547d8572e4bec5357b969458e4c15773e0ba
Christopher Abad <christopher@abad.ml>
2018-11-17 10:22:56 -0800

    .

---
+++ src/sparse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <limits.h>
+
+int main() {
+	size_t n = 0;
+	int ch;
+	while((ch = getchar()) != EOF) {
+		if(ch) {
+			putchar(ch);
+			putchar(n);
+		}
+		n++;
+	}
+	exit(EXIT_SUCCESS);
+}
