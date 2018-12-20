ee9afe087e85ba917b9a5faae1da9f482db88d16
Christopher Abad <christopher@abad.ml>
2018-12-20 05:34:19 -0800

    .

---
+++ web/plot.php
@@ -0,0 +1,16 @@
+<?php
+header("Pragma: no-cache");
+?>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+width="1400"
+height="100"
+viewbox="0 -128 16000 256" preserveAspectRatio="none"
+>
+<rect height="100%" width="100%" fill="whitesmoke" />
+<path d="M 0 0 L 16000 0" stroke="grey" stroke-width="1" />
+<g stroke="blue" stroke-width="2" fill="none">
+        <?php include "./path.txt" ?>
+</g>
+<g stroke="red" stroke-width="2" fill="none">
+</g>
+</svg>
