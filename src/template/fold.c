2c5a8f50e7923d918639c00add587ad097915af8
Christopher Abad <christopher@abad.ml>
2018-12-23 17:59:54 -0900

    .

--- src/template/fold.c
+++ src/template/fold.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <s8.h>
 #include <s8-basic-function.h>
-#ifdef FOLD
+#if defined(FOLD) && defined(FINAL)
 int main(int argc, char **argv) {
-	return fold_function(argc, argv, FOLD);
+	return fold_function(argc, argv, FOLD, FINAL);
 }
 #else
 #error "FOLD macro must be defined and have type <binary_function_t *>"
