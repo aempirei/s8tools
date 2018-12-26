5eafde0fda6d2634e3652c3159ba0ae9715603d6
Christopher Abad <christopher@abad.ml>
2018-12-25 17:29:05 -0900

    .

--- web/plot.php
+++ web/plot.php
@@ -2,13 +2,13 @@
 header("Pragma: no-cache");
 ?>
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-width="1400"
+width="1280"
 height="100"
-viewbox="0 -128 16000 256" preserveAspectRatio="none"
+viewbox="0 -128 1280 256" preserveAspectRatio="none"
 >
 <rect height="100%" width="100%" fill="whitesmoke" />
-<path d="M 0 0 L 16000 0" stroke="grey" stroke-width="1" />
-<g stroke="blue" stroke-width="1" fill="none" transform="translate(27,0)">
+<path d="M 0 0 L 1280 0" stroke="grey" stroke-width="1" />
+<g stroke="blue" stroke-width="1" fill="none" transform="translate(54,0)">
         <?php include "./envelope.txt" ?>
 </g>
 <g stroke="red" stroke-width="1" fill="none">
