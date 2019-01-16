18a9e82a554da1a3014cba6e8f8d2e2dbe6b5c1e
Christopher Abad <christopher@abad.ml>
2019-01-15 16:54:11 -0800

    .

--- src/histogram.c
+++ src/histogram.c
@@ -10,12 +10,33 @@ static inline long int min(long int a, long int b) {
 	return a < b ? a : b;
 }
 
+static long max(long *xs, size_t N) {
+	long x = LONG_MIN;
+	for(size_t n = 0; n < N; n++)
+		if(xs[n] > x)
+			x = xs[n];
+	return x;
+}
+
+static void normalize(long *xs, size_t N) {
+	long x = max(xs, UCHAR_MAX + 1);
+	for(size_t n = 0; n < N; n++) {
+		if(xs[n] != 0) {
+			xs[n] -= 1;
+			xs[n] *= UCHAR_MAX - 1;
+			xs[n] /= x - 1;
+			xs[n] += 1;
+		}
+	}
+}
+
 int main() {
 	long h[UCHAR_MAX + 1] = { 0 };
 	int ch;
 	while((ch = getchar()) != EOF)
 		h[ch]++;
+	normalize(h, UCHAR_MAX + 1);
 	for(ch = 0; ch <= UCHAR_MAX; ch++)
-		putchar(min(h[ch], UCHAR_MAX));
+		putchar(h[ch]);
 	exit(EXIT_SUCCESS);
 }
