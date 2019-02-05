307e79abcae97465f850632881c696cb4f4f02e5
Christopher Abad <christopher@abad.ml>
2019-02-05 07:54:34 -0800

    .

--- src/conj.c
+++ src/conj.c
@@ -19,17 +19,19 @@ static inline int g(int y, int dy) {
 	return (sign(dy) >= 0 ? 1 : -1) * f(y);
 }
 
-int main() {
+int main(int argc, char **argv) {
 	char x[3];
+	char mode = (argc == 2) ? argv[1][0] : '+';
+	int z = mode == '+' ? 1 : mode == '-' ? 1 : 0;
 
 	if(s8_bank_init(x, 3, stdin)) {
 
-		putchar(clip(g(x[1], dx(x+1,2))));
+		putchar(clip(g(x[1], z * dx(x+1,2))));
 
 		while(s8_bank_shift(x, 3, stdin))
-			putchar(clip(g(x[1], dx(x,3))));
+			putchar(clip(g(x[1], z * dx(x,3))));
 
-		putchar(clip(g(x[1], dx(x,2))));
+		putchar(clip(g(x[1], z * dx(x,2))));
 	}
 
 	exit(EXIT_SUCCESS);
